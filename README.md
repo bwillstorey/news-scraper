ec3dde11084ddd98ff4d0f8badb5ee8df1e3d57d
bwillstorey <bwillstorey@gmail.com>
2019-05-29 20:51:52 -0500

    adding bootstrap and additional page styling

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # news-scraper
+
+## Contributors
+@bwillstorey
+
+## Technology
+- MongoDB, Mongoose, Cheerio, Node, Express, Morgan, Axios, Javascript
+- Live Demo: https://protected-ridge-24325.herokuapp.com/
+
+## About
+A web scraper application developed using MongoDB and Node. Users can scrape recent news articles from NPR News grabbing the headlines, direct links and a brief description of each story. Users also have the ability to make  which allows users to read burgers currently in the mySQL database (get), add a new burger to the database (post), or change a burger's state from devoured/not devoured (put).  Application is deployed on Heroku using persistent storage with a mySQL database.
+
+
+## Contributing Guidelines
+All contributions and suggestions are welcome! For direct contributions, please fork the repository and file a pull request.
+
+## Contact
+#### Developer/Full-stack Web Software Developer
+- Homepage: https://brianstorey.me 
+- email: bwillstorey@gmail.com
+- LinkedIn: https://www.linkedin.com/in/brianstorey/
+- AngelList: https://angel.co/brian-storey
--- public/app.js
+++ public/app.js
@@ -1,12 +1,22 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].description + "<br />" + data[i].link + "</p>");
-  }
-});
 
+$(".get-headlines").on("click", () => { 
+
+  location.assign("/scrape");
+
+  $.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles")
+      .append("<p data-id='" + 
+        data[i]._id + "'>" + 
+        "<strong>" + data[i].title + "</strong> <br /> <br />" + 
+        data[i].description + "<br /> <br />" + 
+        data[i].link + "</p> <hr class='my-4' />");
+    }
+  });
+});
 
 // Whenever someone clicks a p tag
 $(document).on("click", "p", function() {
@@ -24,13 +34,15 @@ $(document).on("click", "p", function() {
     .then(function(data) {
       console.log(data);
       // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
+      $("#notes")
+        .append("<div class='card-body'> <h5 class='card-title'>" + 
+         data.title + "</h5>");
       // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      $("#notes").append("<input id='titleinput' name='title' placeholder='Note Title'> <br />");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<textarea id='bodyinput' name='body' placeholder='Note Text'></textarea> <br />");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append("<button type='button' class='btn btn-info' data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
       // If there's a note in the article
       if (data.note) {
@@ -69,4 +81,8 @@ $(document).on("click", "#savenote", function() {
   // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
+});
+
+$('.clear').on("click", () => { 
+  location.assign("/clear");
 });
